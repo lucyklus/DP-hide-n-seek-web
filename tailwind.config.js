b6c0f8b78d5928f0f4070d8c57b5e843de410bf4
Lucia Mandová <lucka.mandova@gmail.com>
2024-02-25 19:48:41 +0100

    :sparkles: FIx prettier, eslint and tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './error.vue',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: '#242038',
+        primary: '#9067C6',
+        secondary: '#8D86C9',
+        tertiary: '#CAC4CE',
+        quartiary: '#F7ECE1',
+        white: '#FFFFFF',
+        black: '#000000',
+      },
+    },
   },
   plugins: [],
-}
-
+};
