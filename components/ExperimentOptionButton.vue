193fe0a0864657cb362f955548dae8c728804523
Lucia Mandová <lucka.mandova@gmail.com>
2024-04-15 18:29:14 +0200

    :sparkles: Add ExperimentOptionButton component

---
+++ components/ExperimentOptionButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="box-border cursor-pointer rounded border-4"
+    :class="[hovering || selected ? 'border-[#7e22ce]' : 'border-transparent']"
+    @mouseover="hovering = true"
+    @mouseleave="hovering = false"
+  >
+    <img :src="imgSrc" :alt="text" width="400" height="400" class="rounded-md" />
+    <p class="text-center mt-2">{{ text }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, ref } from 'vue';
+defineProps({
+  imgSrc: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  selected: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const hovering = ref(false);
+</script>
