a22f8bbd0aed87ef1ac1b8fa98695a733bbe5dc2
Lucia Mandová <lucka.mandova@gmail.com>
2024-04-15 18:25:03 +0200

    :sparkles: Add NextButton component

---
+++ components/NextButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    class="text-white font-bold py-2 px-4 rounded w-full mt-10"
+    :class="[disabled ? 'cursor-not-allowed bg-gray-400' : 'cursor-pointer bg-primary']"
+    @click="onClick()"
+  >
+    Next
+  </button>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  disabled: {
+    type: Boolean,
+    required: false,
+  },
+  onClick: {
+    type: Function,
+    required: true,
+  },
+});
+</script>
